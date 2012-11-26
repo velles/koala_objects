f76886d8d74a0d7c2fe8021c45546da22105c788
Velimir Lesikov <velimirles@gmail.com>
2012-11-26 15:15:26 -0800

    Initial configuration of dependancies

---
+++ lib/koala_objects.rb
@@ -0,0 +1,11 @@
+module KoalaObjects
+  # A Ruby client library for the representing Facebook Graph API objects.
+  # It works directly with koala - ee http://github.com/arsduo/koala
+
+  class KoalaObjectError < StandardError; end
+
+  # Making HTTP requests
+  class << self
+
+  end
+end
